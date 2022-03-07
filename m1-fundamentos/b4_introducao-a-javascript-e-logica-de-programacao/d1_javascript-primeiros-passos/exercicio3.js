20e9060f1a173eac8aae335cf26b3cd023d8279f
gralmeidan <gralmeidan@gmail.com>
2022-03-07 14:38:43 -0400

    Exercicios 2 ao 5

---
+++ m1-fundamentos/b4_introducao-a-javascript-e-logica-de-programacao/d1_javascript-primeiros-passos/exercicio3.js
@@ -0,0 +1,7 @@
+const a = 5
+const b = 10
+const c = 8
+
+if(a > b && a > c) console.log(a)
+else if(b > a && b > c) console.log(b)
+else console.log(c)
---
+++ m1-fundamentos/b4_introducao-a-javascript-e-logica-de-programacao/d1_javascript-primeiros-passos/exercicio5.js
@@ -0,0 +1,10 @@
+const a = 10
+const b = 80
+const c = 90
+
+if((a*b*c) < 0){
+    console.log('valor invalido')
+}
+else {
+    console.log((a+b+c) == 180)
+}
