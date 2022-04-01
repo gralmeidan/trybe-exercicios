4183d1d7f11cf6dff23a874ee328ab65e1259e30
gralmeidan <gralmeidan@gmail.com>
2022-04-01 15:04:12 -0400

    Requisito 2.2

--- m1-fundamentos/b7_introducao-a-javascript-es6-e-unit-tests/d1_let-const-arrow-functions-e-template-literals/part2.js
+++ m1-fundamentos/b7_introducao-a-javascript-es6-e-unit-tests/d1_let-const-arrow-functions-e-template-literals/part2.js
@@ -6,4 +6,17 @@ const exercise1 = (num) => {
   return result
 }
 
-console.log(exercise1(4)) // 24
+// console.log(exercise1(4)) // 24
+
+const exercise2 = (frase) => {
+  const arr = frase.split(' ')
+  let result = ''
+  for (const word of arr) {
+    if(word.length > result.length){
+      result = word
+    }
+  }
+  return result
+}
+
+console.log(exercise2("standing here I realize you are just like me trying to make history but who's to judge right from wrong when our guard is down I think we'll both agree that violence breeds violence but in the end it has to be this way")) // standing
