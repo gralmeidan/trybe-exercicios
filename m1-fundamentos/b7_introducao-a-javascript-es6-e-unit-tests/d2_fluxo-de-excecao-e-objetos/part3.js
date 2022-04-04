f89fec22b6f745e1b894b3058961def11973bca6
gralmeidan <gralmeidan@gmail.com>
2022-04-04 17:23:33 -0400

    Requisitos 3.1 a 3.5

--- m1-fundamentos/b7_introducao-a-javascript-es6-e-unit-tests/d2_fluxo-de-excecao-e-objetos/part3.js
+++ m1-fundamentos/b7_introducao-a-javascript-es6-e-unit-tests/d2_fluxo-de-excecao-e-objetos/part3.js
@@ -17,3 +17,17 @@ const lesson3 = {
   professor: 'Maria Clara',
   turno: 'noite',
 };
+
+const modifyObject = (obj, key, value) => obj[key] = value
+
+modifyObject(lesson2,'turno','noite')
+
+const getKeys = obj => Object.keys(obj)
+
+const getLength = obj => Object.keys(obj).length
+
+const getValues = obj => Object.values(obj)
+
+const allLessons = Object.assign({}, {lesson1}, {lesson2}, {lesson3})
+
+console.log(allLessons);
