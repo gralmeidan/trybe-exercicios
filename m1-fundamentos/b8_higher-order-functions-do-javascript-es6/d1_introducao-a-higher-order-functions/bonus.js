850efa4b12b7bd1b59dc84ea9aa86d29bc9674c5
gralmeidan <gralmeidan@gmail.com>
2022-04-07 15:17:27 -0400

    Requisitos B1.2 e B1.3

--- m1-fundamentos/b8_higher-order-functions-do-javascript-es6/d1_introducao-a-higher-order-functions/bonus.js
+++ m1-fundamentos/b8_higher-order-functions-do-javascript-es6/d1_introducao-a-higher-order-functions/bonus.js
@@ -4,14 +4,30 @@ const mage = {
   healthPoints: 130,
   intelligence: 45,
   mana: 125,
-  damage: undefined,
+  damage: function () {
+    let spentMana, dano;
+    if (this.mana < 15) {
+      spentMana = 0;
+      dano = 'Não possui mana suficiente';
+    } else {
+      spentMana = 15;
+      this.mana -= 15;
+      dano = rand(this.intelligence, this.intelligence * 2);
+    }
+    return {
+      dano,
+      spentMana,
+    };
+  },
 };
 
 const warrior = {
   healthPoints: 200,
   strength: 30,
   weaponDmg: 2,
-  damage: undefined,
+  damage: function () {
+    return rand(this.strength, this.strength * this.weaponDmg);
+  },
 };
 
 const dragon = {
