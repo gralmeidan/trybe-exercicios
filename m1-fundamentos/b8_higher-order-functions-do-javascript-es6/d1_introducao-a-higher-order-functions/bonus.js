e312afb28b3ce69852cbf0a40a12e760b3c28fb1
gralmeidan <gralmeidan@gmail.com>
2022-04-07 15:37:21 -0400

    Requisitos B2

--- m1-fundamentos/b8_higher-order-functions-do-javascript-es6/d1_introducao-a-higher-order-functions/bonus.js
+++ m1-fundamentos/b8_higher-order-functions-do-javascript-es6/d1_introducao-a-higher-order-functions/bonus.js
@@ -5,19 +5,17 @@ const mage = {
   intelligence: 45,
   mana: 125,
   damage: function () {
-    let spentMana, dano;
     if (this.mana < 15) {
-      spentMana = 0;
-      dano = 'Não possui mana suficiente';
+      return {
+        spentMana: 0,
+        dano: 'Não possui mana suficiente',
+      };
     } else {
-      spentMana = 15;
-      this.mana -= 15;
-      dano = rand(this.intelligence, this.intelligence * 2);
+      return {
+        spentMana: 15,
+        dano: rand(this.intelligence, this.intelligence * 2),
+      };
     }
-    return {
-      dano,
-      spentMana,
-    };
   },
 };
 
@@ -39,3 +37,20 @@ const dragon = {
 };
 
 const battleMembers = { mage, warrior, dragon };
+
+const gameActions = {
+  warriorTurn: function (damage) {
+    dragon.healthPoints -= damage();
+  },
+  mageTurn: function (damage) {
+    let attack = damage();
+    dragon.healthPoints -= attack.dano;
+    mage.mana -= attack.spentMana;
+  },
+  dragonTurn: function (damage) {
+    let dano = damage();
+    mage.healthPoints -= dano;
+    warrior.healthPoints -= dano;
+  },
+  getMembers: () => battleMembers,
+};
