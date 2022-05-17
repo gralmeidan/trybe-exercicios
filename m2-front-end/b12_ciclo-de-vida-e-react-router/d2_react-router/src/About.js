11c37a869991c1ff5e5ce7b5cec878c500a43073
gralmeidan <gralmeidan@gmail.com>
2022-05-17 18:08:09 -0400

    Requisitos 4, 5 e 6

---
+++ m2-front-end/b12_ciclo-de-vida-e-react-router/d2_react-router/src/About.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const About = () => (
+  <div className="align-middle border-2 rounded-lg justify-center
+  bg-white m-3 px-4 py-2 min-w-fit">
+    <h2>About</h2>
+    <p>Isso é uma pokedex :)</p>
+  </div>
+)
+
+export default About
--- m2-front-end/b12_ciclo-de-vida-e-react-router/d2_react-router/src/App.js
+++ m2-front-end/b12_ciclo-de-vida-e-react-router/d2_react-router/src/App.js
@@ -1,14 +1,23 @@
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 import Info from "./Info";
 import Pokedex from "./pokedex";
+import About from "./About";
 
 function App() {
   return (
     <BrowserRouter>
       <div>
-        <h1>Pokedex</h1>
+        <header className="flex items-center mt-3">
+          <h1>Pokedex</h1>
+          <div className="border-[1px] h-8 ml-5 mr-2"/>
+          <nav>
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+          </nav>
+        </header>
         <Switch>
           <Route path="/pokemons/:id" component={Info}></Route>
+          <Route path="/about" component={About}></Route>
           <Route path="/" component={Pokedex}></Route>
         </Switch>
       </div>
