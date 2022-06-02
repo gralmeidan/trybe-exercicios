b037bb049b8f07a9aac4e94d51c341d27c4c0534
gralmeidan <gralmeidan@gmail.com>
2022-06-02 15:38:45 -0400

    Requisito 3

--- m2-front-end/b14_testes-automatizados-com-RTL/d2_mocks-e-inputs/service.test.js
+++ m2-front-end/b14_testes-automatizados-com-RTL/d2_mocks-e-inputs/service.test.js
@@ -20,12 +20,25 @@ describe('Testa a função randomNumber', () => {
     expect(r1).not.toBe(r2)
   });
 
-  it('Deve ter sua implementação modificada', () => {
+  it('Deve ter sua implementação modificada para realizar divisões', () => {
     const randomNumber = jest.fn()
       .mockImplementationOnce((a, b) => (a / b));
 
-    expect(randomNumber(4,2)).toBe(2)
-    expect(randomNumber(10,2)).toBe(5)
+    expect(randomNumber(4,2)).toBe(2);
+  });
+
+  it('Deve ter sua implementação modificada para realizar multiplicações', () => {
+    const randomNumber = jest.fn()
+      .mockImplementation((a, b, c) => (a * b * c));
+
+    expect(randomNumber(4,2,2)).toBe(16);
+    expect(randomNumber(10,2,3)).toBe(60);
+
+    randomNumber
+      .mockClear()
+      .mockImplementation((a) => a * 2);
+
+    expect(randomNumber(5)).toBe(10);
   });
 
 })
