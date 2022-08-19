32fc0d7b68679f5700a9e8a4bfa3ff214780251c
gralmeidan <gralmeidan@gmail.com>
2022-08-19 17:56:25 -0400

    Requisito 4

--- m3-back-end/b22_introducao-nodejs/d1_nodejs/bmi.js
+++ m3-back-end/b22_introducao-nodejs/d1_nodejs/bmi.js
@@ -1,6 +1,6 @@
-const { questionInt, questionFloat } = require('readline-sync');
+const { questionFloat } = require('readline-sync');
 
-const weight = questionInt('What\'s your weight? (kg)\n');
+const weight = questionFloat('What\'s your weight? (kg)\n');
 const height = questionFloat('What\'s your height? (m)\n');
 
 console.log(weight / height ^ 2);
