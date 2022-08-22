0451044dd787422cb1e6251329bf3110bb98af02
gralmeidan <gralmeidan@gmail.com>
2022-08-22 17:43:30 -0400

    Requisito 2

--- m3-back-end/b22_introducao-nodejs/d2_restful-apis-com-express/src/app.js
+++ m3-back-end/b22_introducao-nodejs/d2_restful-apis-com-express/src/app.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const activities = require('../data/activities.json');
 const app = express();
 
 app.use(express.json());
+
+app.get('/myActivities/:id', (req, res) => {
+  const activity = activities.find(({ id }) => id == req.params.id);
+  res.status(activity ? 200 : 404).json({ activity });
+});
 
 module.exports = app;
