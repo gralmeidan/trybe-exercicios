a1b62ea4c852d8f6a7cc93cb628dcd2423e6b055
gralmeidan <gralmeidan@gmail.com>
2022-08-23 18:05:22 -0400

    Mocked readfile return

--- m3-back-end/b22_introducao-nodejs/d3_testes-de-integracao/tests/integration/chocolates.test.js
+++ m3-back-end/b22_introducao-nodejs/d3_testes-de-integracao/tests/integration/chocolates.test.js
@@ -1,4 +1,9 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
+const data = require('../mock/chocolates.json');
+const fs = require('fs');
+const sinon = require('sinon');
 
 chai.use(chaiHttp);
+
+sinon.stub(fs.promises, 'readFile').resolves(data);
