e43dc4d57ed6fe7d1a950917954fe1a7abedcad3
gralmeidan <gralmeidan@gmail.com>
2022-08-23 18:10:06 -0400

    Added testes para GET /chocolates/total

--- m3-back-end/b22_introducao-nodejs/d3_testes-de-integracao/tests/integration/chocolates.test.js
+++ m3-back-end/b22_introducao-nodejs/d3_testes-de-integracao/tests/integration/chocolates.test.js
@@ -3,7 +3,19 @@ const chaiHttp = require('chai-http');
 const data = require('../mock/chocolates.json');
 const fs = require('fs');
 const sinon = require('sinon');
+const app = require('../../src/app');
 
+const { expect } = chai;
 chai.use(chaiHttp);
 
 sinon.stub(fs.promises, 'readFile').resolves(data);
+
+describe('Testa o método GET em /chocolates/total', () => {
+  it('Deve retornar o total de chocolates com o status 200', async () => {
+    const response = await chai.request(app).get('/chocolates/total');
+    expect(response.status).to.equal(200);
+    expect(response.body).to.deep.equal({
+      totalChocolates: data.chocolates.length,
+    });
+  });
+});
