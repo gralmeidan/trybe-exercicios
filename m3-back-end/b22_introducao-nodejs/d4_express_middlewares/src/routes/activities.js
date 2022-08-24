b345e3cdf23294d9b5056b7c80570f052fc271a8
gralmeidan <gralmeidan@gmail.com>
2022-08-24 18:00:48 -0400

    Requisito 1.4

--- m3-back-end/b22_introducao-nodejs/d4_express_middlewares/src/routes/activities.js
+++ m3-back-end/b22_introducao-nodejs/d4_express_middlewares/src/routes/activities.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const { validateName } = require('../middlewares/validateActivity');
 const appendData = require('../utils/appendData');
 const activities = express.Router();
+
+activities.use(validateName);
 
 activities.post('/', async (req, res) => {
   const { body } = req;
