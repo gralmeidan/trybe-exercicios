0c72fd45155bc5502d5bb93c58b69e5910959f8a
gralmeidan <gralmeidan@gmail.com>
2022-10-10 17:09:12 -0400

    Requisito 5

---
+++ m3-back-end/s9_oop-e-solid/d1_introducao-a-oop/classes/Data.ts
@@ -0,0 +1,11 @@
+export default class Data {
+  public dia: number;
+  public mes: number;
+  public ano: number;
+
+  constructor(dia: number, mes: number, ano: number) {
+    this.dia = dia;
+    this.mes = mes;
+    this.ano = ano;
+  }
+}
