5397c1161ba490ee6bd241cd83866767f3b766b6
gralmeidan <gralmeidan@gmail.com>
2022-10-13 17:35:19 -0400

    Exercicio 5

--- m3-back-end/s9_oop-e-solid/d2_heranca-e-interface/classes/Employee.ts
+++ m3-back-end/s9_oop-e-solid/d2_heranca-e-interface/classes/Employee.ts
@@ -6,12 +6,43 @@ export default class Employee extends Person {
   constructor(
     name: string,
     birthDate: Date,
-    public salary: number,
-    public admissionDate: Date
+    private _salary: number,
+    private _admissionDate: Date
   ) {
     super(name, birthDate);
+
+    this.validateSalary(_salary);
+    this.validateAdmissionDate(_admissionDate);
+
     this.registration = this.generateRegistration();
   }
 
+  private validateSalary = (salary: number) => {
+    if (salary < 0) throw new Error("Salary must be positive");
+  };
+
+  private validateAdmissionDate = (date: Date) => {
+    if (date > new Date())
+      throw new Error("AdmissionDate can't be bigger than current Date");
+  };
+
   private generateRegistration = () => new Date(Date.now()).toISOString();
+
+  set salary(sal: number) {
+    this.validateSalary(sal);
+    this._salary = sal;
+  }
+
+  get salary() {
+    return this._salary;
+  }
+
+  set admissionDate(date: Date) {
+    this.validateAdmissionDate(date);
+    this._admissionDate = date;
+  }
+
+  get admissionDate() {
+    return this._admissionDate;
+  }
 }
